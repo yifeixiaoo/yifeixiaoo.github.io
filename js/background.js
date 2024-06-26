4cf2a540dbc0498e4e3fcfa6bf6d2f26d74cd65f
Yifei Xiao <yifeixiaoo@gmail.com>
2024-06-25 22:20:22 -0700

    add smooth transition effect into photo

--- js/background.js
+++ js/background.js
@@ -1,24 +1,25 @@
 const accessKey = 'kYpiX_AGhUF1cQL8IQYkOyLXYaccfQfvR6Umf_wlt6I';
 const query = 'dark landscape';
-const imageWidth = 1920; 
-const imageHeight = 1080; 
+const imageWidth = 1920;
+const imageHeight = 1080;
 
 function setUnsplashBackground(imageUrl, creator) {
-    // Apply the Unsplash image
-    document.body.style.backgroundImage = `url(${imageUrl})`;
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundRepeat = "no-repeat";
-    document.body.style.backgroundAttachment = "fixed";
+    const backgroundPic = document.getElementById('background_pic');
 
-    // Update the author information
-    document.getElementById("author").textContent = creator;
+    const img = new Image();
+    img.src = imageUrl;
+    img.onload = () => {
+        backgroundPic.style.setProperty('--background-image-url', `url('${imageUrl}')`);
+        backgroundPic.classList.add('loaded');
+        document.getElementById('author').textContent = creator;
+    };
 }
 
 function unsplashImage() {
     fetch(`https://api.unsplash.com/photos/random?query=${query}&client_id=${accessKey}`)
         .then((response) => response.json())
         .then((data) => {
-          const imageUrl = `${data.urls.raw}&fit=crop&auto=format&w=${imageWidth}&h=${imageHeight}`;
+            const imageUrl = `${data.urls.raw}&fit=crop&auto=format&w=${imageWidth}&h=${imageHeight}`;
             const creator = data.user.name;
 
             setUnsplashBackground(imageUrl, creator);
@@ -29,13 +30,14 @@ function unsplashImage() {
 }
 
 const imageArray = [
-    './contents/background1.jpg',
-    './contents/background2.jpg',
-    './contents/background3.jpg',
-    './contents/background4.jpg',
-    './contents/background5.jpg',
-    './contents/background6.jpg',
+    'https://yifeixiaoo.github.io/contents/background1.jpg',
+    'https://yifeixiaoo.github.io/contents/background2.jpg',
+    'https://yifeixiaoo.github.io/contents/background3.jpg',
+    'https://yifeixiaoo.github.io/contents/background4.jpg',
+    'https://yifeixiaoo.github.io/contents/background5.jpg',
+    'https://yifeixiaoo.github.io/contents/background6.jpg',
 ];
+
 const creatorArray = ['Thanos Pal', 'Anders Jilden', 'Ken Cheung', 'Andre Benz', 'Marek Piwnicki', 'Daniel Leone'];
 
 function random_image() {
